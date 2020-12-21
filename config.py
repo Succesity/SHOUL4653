abefa621ba9632ea9c665d7010aba170a5bc7fb9
Succesity <67516972+Succesity@users.noreply.github.com>
2020-12-21 08:03:13 -0300

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ client_id = 739501453059817542
 
 prefix = 'bv!'
 
-owner_id = 739501453059817542
+owner_id = 405339224125210625
 
 token = "NzM5NTAxNDUzMDU5ODE3NTQy.XybYZQ.wq6N9qrXDSgX4lZFNx_7I7eCf8Q"
